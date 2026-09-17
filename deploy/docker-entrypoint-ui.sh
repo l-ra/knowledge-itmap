e8fd0bb1b55891a80feb509f8e2cad0cf45cae98
lra <lra@r73.info>
2026-09-17 16:09:56 +0200

    build fix

--- deploy/docker-entrypoint-ui.sh
+++ deploy/docker-entrypoint-ui.sh
@@ -2,7 +2,9 @@
 set -eu
 : "${KC_UPSTREAM:=http://kc-knowledge-core:8080}"
 : "${MCP_UPSTREAM:=http://127.0.0.1:3100}"
+mkdir -p /tmp/nginx/client_temp /tmp/nginx/proxy_temp \
+  /tmp/nginx/fastcgi_temp /tmp/nginx/uwsgi_temp /tmp/nginx/scgi_temp
 envsubst '${KC_UPSTREAM} ${MCP_UPSTREAM}' \
   < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
-exec nginx -g 'daemon off;'
+exec nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
